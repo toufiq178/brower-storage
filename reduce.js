0260036aad6b28736e4eae2756e2363a9c31e14b
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-03-10 20:35:37 +0600

    reduce task more

--- reduce.js
+++ reduce.js
@@ -18,9 +18,9 @@
 
 
 // Task: Find max number
-const numbers = [5, 12, 8, 20, 3];
+// const numbers = [5, 12, 8, 20, 3];
 
-const maxNumber = numbers.reduce((a , b) => a > b ? a : b , 0)
+// const maxNumber = numbers.reduce((a , b) => a > b ? a : b , 0)
 
 // console.log(maxNumber);
 
@@ -38,27 +38,77 @@ const maxNumber = numbers.reduce((a , b) => a > b ? a : b , 0)
 
 
 // Task: Count occurrences
-const fruits = ["apple","banana","apple","orange","banana","apple"];
+// const fruits = ["apple","banana","apple","orange","banana","apple"];
 
-const count = fruits.reduce((accumulator , currentValue) => {
+// const count = fruits.reduce((accumulator , currentValue) => {
 
-     accumulator[currentValue] = (accumulator[currentValue] || 0) + 1
+//      accumulator[currentValue] = (accumulator[currentValue] || 0) + 1
 
-     return accumulator
-} , {})
+//      return accumulator
+// } , {})
 
-// console.log(count);
+// // console.log(count);
 
 
 
-const cart = [
-  {name:"shirt", price:500},
-  {name:"pant", price:800},
-  {name:"shoes", price:1200}
+// const cart = [
+//   {name:"shirt", price:500},
+//   {name:"pant", price:800},
+//   {name:"shoes", price:1200}
+// ];
+
+
+// const totalPrice = cart.reduce((acc , curr) => acc + curr.price , 0)
+
+// console.log(totalPrice);
+
+
+
+// const words = ["apple", "banana", "mango", "orange"];
+
+// const totalCharacter = words.reduce((acc , w) => acc + w.length  , 0)
+
+// console.log(totalCharacter);
+
+
+// const cart = [
+//   {name:"shirt", price:500},
+//   {name:"pant", price:800},
+//   {name:"shoes", price:1200},
+//   {name:"cap", price:200}
+// ];
+
+// const totalPrice = cart.reduce((acc , cart) => acc + cart.price , 0);
+
+// console.log(totalPrice);
+
+
+// const numbers = [10, 45, 22, 90, 5, 60];
+
+// const maxNumber = numbers.reduce((acc , num) => Math.max(acc , num) , 0)
+
+// // console.log(maxNumber);
+
+// const bigNum = Math.max(...numbers)
+
+// console.log(bigNum);
+
+
+const users = [
+  {id:1, name:"Taj"},
+  {id:2, name:"Rahim"},
+  {id:3, name:"Karim"}
 ];
 
 
-const totalPrice = cart.reduce((acc , curr) => acc + curr.price , 0)
+const obj  = users.reduce((acc , user ) => {
 
-console.log(totalPrice);
 
+  acc[user.id] = user.name
+  
+  return  acc
+
+} , {})
+
+
+console.log(obj);
